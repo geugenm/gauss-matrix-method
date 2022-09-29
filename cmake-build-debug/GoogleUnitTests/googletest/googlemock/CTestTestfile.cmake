8fe493360bd1b90e1346db6d1de3b8db227d4cad
Eugene Gleba <glebajk@gmail.com>
2022-09-29 21:47:56 +0300

    Deleted unneccessary directories

--- cmake-build-debug/GoogleUnitTests/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/s/labs/computational-methods/lab-1/GoogleUnitTests/googletest/googlemock
-# Build directory: /mnt/s/labs/computational-methods/lab-1/cmake-build-debug/GoogleUnitTests/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/GoogleUnitTests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/s/labs/computational-methods/lab-1/GoogleUnitTests/googletest/googletest
-# Build directory: /mnt/s/labs/computational-methods/lab-1/cmake-build-debug/GoogleUnitTests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
